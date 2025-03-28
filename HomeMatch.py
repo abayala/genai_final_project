74995103d88dbbfb344c9f0c10965ccaafac04af
Abraham Ayala Hernandez <abrahamayalah@gmail.com>
2025-03-28 19:35:45 +0100

    first commit

---
+++ HomeMatch.py
@@ -0,0 +1,9 @@
+#A starter file for the HomeMatch application if you want to build your solution in a Python program instead of a notebook. 
+
+import os
+
+os.environ["OPENAI_API_KEY"] = "voc-639216515126677374835767814980559d06.80243505"
+os.environ["OPENAI_API_BASE"] = "https://openai.vocareum.com/v1"
+
+from langchain.llms import OpenAI
+
